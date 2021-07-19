35cf0fb460ab3f255514ff88db4d3b3adc698c8d
anoopjoy999 <65029416+anoopjoy999@users.noreply.github.com>
2021-07-19 13:14:15 +0530

    Committing
    Committing

---
+++ src/app/services/upload-files.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadFilesService {
+
+  private baseUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+
+  upload(file: File): Observable<HttpEvent<any>> {
+    const formData: FormData = new FormData();
+
+    formData.append('file', file);
+
+    const req = new HttpRequest('POST', `${this.baseUrl}/upload`, formData, {
+      reportProgress: true,
+      responseType: 'json'
+    });
+
+    return this.http.request(req);
+  }
+
+  getFiles(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/files`);
+  }
+
+  getFile(id:String): Observable<any> {
+
+    return this.http.post(`${this.baseUrl}/file`,{id:id},{ responseType: 'blob'});
+  }
+
+  getTemplate(template:String): Observable<any> {
+
+    return this.http.post(`${this.baseUrl}/template`,{template:template}, { responseType: 'blob'});
+  }
+}
